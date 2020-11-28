b86ac2d1f7b41694e6fcf932aabbe4b69d9f5af4
Yongkang Zhao <cookieyong@gmail.com>
2020-11-28 14:09:10 -0800

    improved sign in/out process with visual buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -21,13 +21,31 @@ export class GoogleAuth extends Component {
         })
     };
 
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton(){
         if(this.state.isSignedIn === null){
-            return <div>I dont know if we are signed in</div>
+            return null;
         } else if (this.state.isSignedIn){
-            return <div>i am signed in</div>
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                        Sign Out
+                </button>
+            )
         } else {
-            return <div>I am not signed in</div>
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            )
         }
     }
 

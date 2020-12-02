c328655f31d86f709b639c3fc332398c4d0da754
Yongkang Zhao <cookieyong@gmail.com>
2020-12-02 14:32:00 -0800

    created history.js to manually control navigation so user can create stream and be sent to stream list page once new stream list item is created

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
+
+
 
 export class StreamList extends Component {
     componentDidMount(){
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream){
+        if (stream.userId === this.props.currentUserId){
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
 
     renderList(){
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -22,19 +40,37 @@ export class StreamList extends Component {
         })
     }
 
+    renderCreate(){
+        if (this.props.isSignedIn){
+            return (
+                <div style={{textAlign:'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
+
     render() {
         console.log(this.props.streams);
         return (
            <div>
                <h2>Now Streaming</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
            </div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) };
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 }
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
---
+++ client/src/history.js
@@ -0,0 +1,9 @@
+import { createBrowserHistory } from 'history'; 
+
+
+
+
+
+
+
+export default createBrowserHistory();

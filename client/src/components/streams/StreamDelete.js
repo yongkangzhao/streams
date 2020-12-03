92c7a66e8af408388dee77df26e6665c5bdc433e
Yongkang Zhao <cookieyong@gmail.com>
2020-12-02 22:12:56 -0800

    implemetent a basic structure for delete compoent which is going to be rendered as a modal over the streamlist component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Modal from '../Modal';
+
 
 export class StreamDelete extends Component {
+    actions = (
+        <Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </Fragment>
+    )
+
     render() {
+
+
+
         return (
             <div>
                 StreamDelete
+                <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={this.actions}/>
             </div>
         )
     }

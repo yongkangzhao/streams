f68ce284eb816abafc2359ea37cab2c8d9adfce1
Yongkang Zhao <cookieyong@gmail.com>
2020-12-02 22:52:24 -0800

    finished implementing stream delete component as a modal to the page, also will close it self if user clicks outside of the modal prompt.

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,26 +1,40 @@
 import React, { Component, Fragment } from 'react'
 import Modal from '../Modal';
+import { Link } from 'react-router-dom';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../actions';
 
 
 export class StreamDelete extends Component {
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+
     actions = (
         <Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
+            <button onClick={()=>{this.props.deleteStream(this.props.match.params.id)}} className="ui button negative">Delete</button>
+            <Link to="/" className="ui button">Cancel</Link>
+            
+            
         </Fragment>
     )
 
     render() {
-
-
-
-        return (
-            <div>
-                StreamDelete
-                <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={this.actions}/>
-            </div>
-        )
+        if (this.props.stream){
+            return (
+                    <Modal title="Delete Stream" content={`Are you sure you want to delete this stream: ${this.props.stream.title} ?`} actions={this.actions} onDismiss={()=>history.push('/')}/>
+            )
+        }
+        return null;
     }
 }
 
-export default StreamDelete
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
+};
+
+export default connect(mapStateToProps,{fetchStream, deleteStream})(StreamDelete);

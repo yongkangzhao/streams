2434f637ffbaf73b2f732d8afea6220308377a8d
Yongkang Zhao <cookieyong@gmail.com>
2020-12-03 01:02:27 -0800

    edited json api server to be able to be accessed from anywhere instead of only localhost still using port 3001

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://10.164.173.122:3001'
 })
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -4,7 +4,8 @@ import {
     FETCH_STREAM,FETCH_STREAMS,CREATE_STREAM,EDIT_STREAM,DELETE_STREAM
 } from '../actions/types';
 
-export default (state = {}, action) => {
+
+const streamReducer = (state = {}, action) => {
     switch (action.type){
         case FETCH_STREAM:
         case CREATE_STREAM:
@@ -25,3 +26,5 @@ export default (state = {}, action) => {
             return state;
     }
 };
+
+export default streamReducer;

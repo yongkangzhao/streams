e79ca6ee92b71c0baaddaed316d9a01a3dd358c8
Yongkang Zhao <cookieyong@gmail.com>
2020-12-02 23:10:11 -0800

    connected redux to streamshow

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 export class StreamShow extends Component {
+
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
     render() {
-        return (
-            <div>
-                StreamShow
-            </div>
-        )
+        if(this.props.stream){
+            return (
+                <div>
+                    <h1>
+                        {this.props.stream.title}
+                    </h1>
+
+                    <h5>
+                        {this.props.stream.description}
+                    </h5>
+                </div>
+            )
+        }
+        return null;
     }
 }
 
-export default StreamShow
+const mapStatetoProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
+};
+
+export default connect(mapStatetoProps, {fetchStream})(StreamShow);

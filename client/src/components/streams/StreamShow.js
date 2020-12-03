035923d955281ddd9459a6935b5c5d160f3c21c5
Yongkang Zhao <cookieyong@gmail.com>
2020-12-03 13:47:31 -0800

    attached video player to the streamshow page and handled component did mount and willunmount scenarios, so the player will display and load stream correctly, also will stop and unmount when client navigates away from the page.

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 export class StreamShow extends Component {
 
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
+
+
     componentDidMount(){
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
     }
+
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    componentWillUnmount(){
+        this.flvPlayer.destroy();
+    } 
+
+
+    buildPlayer(){
+        if(this.flvPlayer || !this.props.stream){
+            return;
+        }
+        this.flvPlayer = flv.createPlayer({
+            type: 'flv',
+            url: `http://10.164.173.122:8000/live/${this.props.match.params.id}.flv`
+        });
+        this.flvPlayer.attachMediaElement(this.videoRef.current);
+        this.flvPlayer.load();
+        this.flvPlayer.play();
+    }
+
 
     render() {
         if(this.props.stream){
             return (
                 <div>
+                    <video ref={this.videoRef} style={{width:'100%'}} controls/>
                     <h1>
                         {this.props.stream.title}
                     </h1>
